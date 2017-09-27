24543e43f9a7667e788a6969b199c592522a6306
Michael E. Chancey Jr <chancey@Michaels-MacBook-Pro.local>
2017-09-27 01:06:49 -0400

    Added sample.config.js for CircleCi

---
+++ sample.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // defaultPanel: 0,
+  panels: [
+    {
+      name: 'Panel 1',
+      components: [
+        {
+          name: 'TimeOfDayGreeting',
+          options: {
+            greetings: ['Good Morning', 'Good Afternoon', 'Good Evening'],
+          },
+        },
+      ],
+    },
+  ],
+};
